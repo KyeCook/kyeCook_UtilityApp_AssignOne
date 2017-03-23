798ec75a5003d98afdfb588d75fba2e0136f07c8
kyecook <acfanatics@hotmail.com>
2017-03-23 12:36:12 +1000

    Added in shared preferences

--- app/src/main/java/com/example/kyecook/kyecook_utilityapp/MainActivity.java
+++ app/src/main/java/com/example/kyecook/kyecook_utilityapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.kyecook.kyecook_utilityapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -9,20 +11,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+//todo make settings go into top menu bar using <menu> within xml file
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView countryToBeConverted;
+    private TextView countryToBeConvertedString;
+
     private TextView convertedCurrency;
     private EditText moneyToConvert;
-    private float currency;
+    private double currency;
+
+    private SharedPreferences preferences;
+
+//    TODO use shared preferences to set country/currency that is to be converted
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        preferences = getSharedPreferences("countryPreferences", MODE_PRIVATE);
+
 
         countryToBeConverted = (TextView) findViewById(R.id.countryToBeConverted);
+        countryToBeConvertedString = (TextView) findViewById(R.id.countryToBeConvertedString);
         convertedCurrency = (TextView) findViewById(R.id.convertedCurrency);
         moneyToConvert = (EditText) findViewById(R.id.moneyToConvertInput);
 
@@ -42,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
                     currency = Integer.parseInt(s.toString());
 
-                    convertedCurrency.setText(String.valueOf("$" + currency));
+//                    convertedCurrency.setText(String.valueOf("$" + currency));
 
                 } catch (Exception e) {
 
@@ -55,14 +68,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         conversionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                This is the segment for the conversion button. It loads the 'convert' method.
 
+                double currencyToConvert = Double.parseDouble(preferences.getString("countryCurrency",""));
+
+                String convertedCurrencyString = '$' + String.valueOf(currency * currencyToConvert);
+
+                convertedCurrency.setText(convertedCurrencyString);
 
             }
         });
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        countryToBeConverted.setText(preferences.getString("currency", ""));
+        countryToBeConvertedString.setText(preferences.getString("country", ""));
+
+    }
+
+    public void settingsHandler(View view) {
+        Intent intent = new Intent(this, settingsActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/kyecook/kyecook_utilityapp/settingsActivity.java
@@ -0,0 +1,89 @@
+package com.example.kyecook.kyecook_utilityapp;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class settingsActivity extends AppCompatActivity {
+
+    String countryToConvert;
+    String currencyToConvert;
+    String countryCurrency;
+
+    private SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        preferences = getSharedPreferences("countryPreferences", MODE_PRIVATE);
+
+        Button usdButton = (Button) findViewById(R.id.changeToUSD);
+        Button euruButton = (Button) findViewById(R.id.changeToEUR);
+
+//        Handles USD button operation - sets American values to preferences to be used by main Activity
+        usdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                countryToConvert = "America";
+                currencyToConvert = "USD";
+                countryCurrency = "1.29";
+
+                preferences.edit().putString("country", countryToConvert).apply();
+                preferences.edit().putString("currency", currencyToConvert).apply();
+                preferences.edit().putString("countryCurrency", countryCurrency).apply();
+
+
+            }
+        });
+
+        euruButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                countryToConvert = "Euro Currency";
+                currencyToConvert = "EURO";
+                countryCurrency = "1.41";
+
+                preferences.edit().putString("country", countryToConvert).apply();
+                preferences.edit().putString("currency", currencyToConvert).apply();
+                preferences.edit().putString("countryCurrency", countryCurrency).apply();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+//        Sets preferences value to remain when activity is 'stopped'
+
+        preferences.edit().putString("country", countryToConvert).apply();
+        preferences.edit().putString("currency", currencyToConvert).apply();
+        preferences.edit().putString("countryCurrency", countryCurrency).apply();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        countryToConvert = "United States Dollars";
+        currencyToConvert = "USD";
+        countryCurrency = "1.29";
+
+        preferences.edit().putString("country", countryToConvert).apply();
+        preferences.edit().putString("currency", currencyToConvert).apply();
+        preferences.edit().putString("countryCurrency", countryCurrency).apply();
+    }
+
+    public void backHandler(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
